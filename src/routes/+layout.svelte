7d3b4695210fbc75bbd91af521109ffb88962e11
Thien Huynh <thien1367526@gmail.com>
2023-02-07 15:31:03 +0700

    Initialize supabase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
-<script>
-   import "../app.css";
+<script lang="ts">
+	import { supabase } from "$lib/config/supabase";
+	import { invalidate } from "$app/navigation";
+	import { onMount } from "svelte";
+	import "./styles.css";
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate("supabase:auth");
+		});
+
+		return () => subscription.unsubscribe();
+	});
 </script>
 
 <slot />
