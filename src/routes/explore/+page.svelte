4de007e066fd6830d3c1925b117be82c4db26a27
Thien Huynh <thien1367526@gmail.com>
2023-05-03 20:59:56 +0700

    Add queries for getting books, readers

---
+++ src/routes/explore/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import BookCard from "$lib/components/BookCard.svelte";
+	import { getContextClient, queryStore } from "@urql/svelte";
+   import { GetBooksDocument } from "$lib/graphql/generated.d";
+
+   const store = queryStore({
+      client: getContextClient(),
+      query: GetBooksDocument,
+   });
+
+   $: ({ data, fetching } = $store);
+</script>
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
+<!-- <BookCard imgSrc={data?.books[0].cover_url ?? ""} /> -->
